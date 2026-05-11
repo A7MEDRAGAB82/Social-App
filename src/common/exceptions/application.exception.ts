4c70627c177d59f65696f26be7484ab05afc2d27
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2026-05-11 14:08:17 +0300

    feat(shared): complete exceptions and success response

--- src/common/exceptions/application.exception.ts
+++ src/common/exceptions/application.exception.ts
@@ -1,6 +1,11 @@
+interface AError {
+    statusCode: number;
+    message: string;
+    cause?: unknown;
+    stack?: string;
+}
 
-
-class ApplicationException extends Error {
+export class ApplicationException extends Error implements AError {
     constructor(message: string , public statusCode: number = 500, cause: unknown) {
         super(message , { cause });
         this.name = this.constructor.name;
@@ -11,4 +16,20 @@ class ApplicationException extends Error {
     }
 }
 
-export default ApplicationException;
+export class BadRequestException extends ApplicationException {
+    constructor(message: string, cause?: unknown) {
+        super(message, 400, {cause});
+    }
+}
+
+export class conflictException extends ApplicationException {
+    constructor(message: string, cause?: unknown) {
+        super(message, 409, {cause});
+    }
+}
+
+export class NotFoundException extends ApplicationException {
+    constructor(message: string, cause?: unknown) {
+        super(message, 404, {cause});
+    }
+}

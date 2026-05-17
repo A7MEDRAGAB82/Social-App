88f15e5cf935ea497cc54f473c2750911af496a3
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2026-05-17 19:15:39 +0300

    feat(database): implement database connection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,15 @@ import type { Express, Request, Response } from "express";
 import { authRouter } from "./modules/auth";
 import { errorMiddleware } from "./middleware/error.middleware";
 import {env} from "./config/env.service";
+import DBconnection from "./database/connection";
 
 
 export const bootstrap = () => {
     const app : Express = express();
 
+    
     app.use(express.json());
+    DBconnection();
     app.use("/auth", authRouter)
   app.use(errorMiddleware);
     app.listen(env.port, () => {

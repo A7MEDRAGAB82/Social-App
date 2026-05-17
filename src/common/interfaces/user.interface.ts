586035035c99248b5468db8f67358a86bfaecfa8
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2026-05-17 21:00:33 +0300

    feat(database): implement userModel and initialize interfaces - Enums

---
+++ src/common/interfaces/user.interface.ts
@@ -0,0 +1,17 @@
+import { GenderEnum  , RoleEnum , ProviderEnum } from "../enums";
+
+export interface IUser extends Document {
+    username: string;
+    firstName?: string;
+    lastName?: string;
+    email: string;
+    password?: string; 
+    gender: GenderEnum ;
+    role: RoleEnum;
+    phoneNumber?: string;
+    profilePicture?: string;
+    profileCoverPicture?: string;
+    provider: ProviderEnum;
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ src/database/models/user.model.ts
@@ -0,0 +1,48 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { IUser } from "../../common/interfaces/user.interface";
+import { GenderEnum, ProviderEnum, RoleEnum } from "../../common/enums";
+
+
+
+
+
+
+const userSchema = new Schema<IUser>({
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: function(this: IUser) { return this.provider === ProviderEnum.LOCAL; } },
+    gender: { 
+        type: String, 
+        enum: Object.values(GenderEnum), 
+        default: GenderEnum.MALE 
+    },
+    role: { 
+        type: String, 
+        enum: Object.values(RoleEnum), 
+        default: RoleEnum.USER 
+    },
+    provider: { 
+        type: String, 
+        enum: Object.values(ProviderEnum), 
+        default: ProviderEnum.LOCAL 
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now } 
+}, {
+    timestamps: true ,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+});
+
+userSchema.virtual('username').set(function(this: IUser) {
+    let fullName = "";
+    if (this.firstName) fullName += this.firstName;
+    if (this.lastName) fullName += ` ${this.lastName}`;
+    return fullName.trim();
+}).get(function(this: IUser) {
+    return `${this.firstName} ${this.lastName}`;
+});
+
+
+ const UserModel = mongoose.model<IUser>('User', userSchema);
+ export default UserModel;
